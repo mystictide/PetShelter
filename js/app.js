54b274ad7ee90ad735f81825c37089da4d744318
Furkan Güler <furkang2k20@outlook.com>
2021-12-22 18:54:52 +0300

    Add project files.

---
+++ js/app.js
@@ -0,0 +1,6 @@
+﻿const navtoggle = document.getElementsByClassName('nav-toggle')[0]
+const navbarlinks = document.getElementsByClassName('navbar-links')[0]
+
+navtoggle.addEventListener('click', () => {
+    navbarlinks.classList.toggle('active')
+})
